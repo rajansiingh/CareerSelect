139186bebbd7e80813f3e24cb67d02e3098fe450
smriti chawla <smriti.chawla@tothenew.com>
2020-06-21 13:40:38 +0530

    listing page styling

--- src/components/common/Listing/Listing.js
+++ src/components/common/Listing/Listing.js
@@ -2,7 +2,9 @@ import React, { Fragment } from "react"
 import { Link } from "gatsby"
 import * as dayjs from "dayjs"
 import { blogURI, authorURI, categoryURI, tagsURI } from "../../../../globals"
-import FluidImage from "../../Image/FluidImage"
+import FluidImage from "../../Image/FluidImage";
+
+import './styles.css';
 
 const Listing = ({ data, type = [] }) => {
 
@@ -23,26 +25,30 @@ const Listing = ({ data, type = [] }) => {
   const withCategory = type.indexOf("withCategory") > -1 || true
   const withDate = type.indexOf("withDate") > -1 || true
   const displayDate = dayjs(date).format("MMM DD, YYYY")
-  const List = (<Fragment>
-    <header>
-      <Link to={`${blogURI}/${uri}/`}>
-        <h2 style={{ marginBottom: "5px" }}>{title}</h2>
-        {featuredImage && <FluidImage image={featuredImage} style={{ margin: 0 }}/>}
-      </Link>
-    </header>
-    {withTags && tags && nodeMapper(tags.nodes, tagsURI)}
-    {withDesc && excerpt && <div dangerouslySetInnerHTML={{ __html: excerpt }}/>}
-    {withAuthor && author && <div>
-      By:
-      <Link to={`${authorURI}/${author.slug}/`}>
-        {author.name}
-      </Link>
-    </div>}
-    {withCategory && categories && nodeMapper(categories.nodes, categoryURI)}
-    {withDate && date && displayDate}
-  </Fragment>)
+
+  const List = <div className='listing'>
+      <header>
+        <Link to={`${blogURI}/${uri}/`}>
+          {featuredImage && <FluidImage image={featuredImage} style={{ margin: 0 }}/>}
+          <h2>{title}</h2>
+        </Link>
+      </header>
+    <div className='list-desc'>
+      {withDate && date && displayDate}
+      {withDesc && excerpt && <div className='job-desc' dangerouslySetInnerHTML={{ __html: excerpt }}/>}
+      {withAuthor && author && <div>
+        By:
+        <Link to={`${authorURI}/${author.slug}/`}>
+          {author.name}
+        </Link>
+      </div>}
+      {/*   {withCategory && categories && nodeMapper(categories.nodes, categoryURI)}*/}
+      <div className='tags'>{withTags && tags && nodeMapper(tags.nodes, tagsURI)}</div>
+    </div>
+  </div>;
+
   return (
-    <div style={{ marginBottom: "30px" }}>
+    <div className='listing-page row'>
       {List}
     </div>
   )
